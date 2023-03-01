2e2f37aa4d0a060620a7b499d9475f15592ed7fe
maxefimov <efimov.academy.top@gmail.com>
2023-03-01 17:18:46 +0300

    Добавьте файлы проекта.

---
+++ FPBinaryOperationsProject/FPBinaryOperationsProject.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0b10110101; // 181
+    int b = 0b11010010; // 210
+
+    /*cout << a + b << "\n";
+    cout << a * b << "\n";
+
+    cout << (a & b) << "\n";
+    cout << (a | b) << "\n";
+    cout << (a ^ b) << "\n";
+
+    cout << (a << 2) << "\n";
+    cout << (a >> 2) << "\n";*/
+
+    int n = 100;
+
+    //cout << (n << 1) << "\n"; // x2
+    //cout << (n << 2) << "\n"; // x4
+    //cout << (n << 3) << "\n"; // x8
+    //cout << (n << 4) << "\n"; // x16
+
+    //cout << (n >> 1) << "\n"; // /2
+    //cout << (n >> 2) << "\n"; // /4
+    //cout << (n >> 3) << "\n"; // /8
+    //cout << (n >> 4) << "\n"; // /16
+    
+    int m = -345;
+    cout << (~m + 1);
+}
