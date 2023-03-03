481216fc83974db9eb46d356dde2e6e3f10bf034
maxefimov <efimov.academy.top@gmail.com>
2023-03-03 15:24:54 +0300

    03-02-23

--- FPBinaryOperationsProject/FPBinaryOperationsProject.cpp
+++ FPBinaryOperationsProject/FPBinaryOperationsProject.cpp
@@ -1,11 +1,12 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main()
+void BitOperationsBase()
 {
-    int a = 0b10110101; // 181
-    int b = 0b11010010; // 210
+    //int a = 0b10110101; // 181
+    //int b = 0b11010010; // 210
 
     /*cout << a + b << "\n";
     cout << a * b << "\n";
@@ -28,7 +29,31 @@ int main()
     //cout << (n >> 2) << "\n"; // /4
     //cout << (n >> 3) << "\n"; // /8
     //cout << (n >> 4) << "\n"; // /16
-    
-    int m = -345;
+
+    int m = 345;
     cout << (~m + 1);
+}
+
+int main()
+{
+    //BitOperationsBase();
+
+    /*bool weapon[8];
+    weapon[1] = true;*/
+
+    char weapon{};
+
+    char mask = (1 << 2);
+    weapon = weapon | mask;
+    mask = (1 << 5);
+    weapon = weapon | mask;
+    mask = 1;
+    weapon = weapon | mask;
+
+    cout << (int)weapon << "\n";
+
+    mask = ~(1 << 2);
+    weapon = weapon & mask;
+
+    cout << (int)weapon << "\n";
 }
